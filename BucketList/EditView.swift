7cbbfe03b0ad877925e3d50827cdd3f587dc9f9c
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-11-07 18:50:39 -0600

    Sorting Wikipedia results

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -42,7 +42,7 @@ struct EditView: View {
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ") +
-                            Text("Descripción de la página aquí")
+                            Text(page.description)
                                 .italic()
                         }
                     case .loading:
@@ -93,7 +93,7 @@ struct EditView: View {
             let items = try JSONDecoder().decode(Result.self, from: data)
 
             // success – convert the array values to our pages array
-            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            pages = items.query.pages.values.sorted()
             loadingState = .loaded
         } catch {
             // if we're still here it means the request failed somehow
