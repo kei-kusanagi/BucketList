acfdaec500219fb070e28074d0349e1cc13193b7
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-11-11 17:57:03 -0600

    day 73 - Bucket List: Wrap up challenge 3
    Create another view model, this time for EditView. What you put in the view model is down to you, but I would recommend leaving dismiss and onSave in the view itself – the former uses the environment, which can only be read by the view, and the latter doesn’t really add anything when moved into the model.

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -11,34 +11,24 @@ import SwiftUI
 
 struct EditView: View {
     
-    enum LoadingState {
-        case loading, loaded, failed
-    }
-    
     var onSave: (Location) -> Void
 
     @Environment(\.dismiss) var dismiss
-    var location: Location
+    
+    @State private var viewModel: ViewModel
 
-    @State private var name: String
-    @State private var description: String
-
-    
-    @State private var loadingState = LoadingState.loading
-    @State private var pages = [Page]()
-    
-    
+   
     var body: some View {
         NavigationStack {
             Form {
                 Section {
-                    TextField("Nombre del lugar", text: $name)
-                    TextField("Descripción", text: $description)
+                    TextField("Nombre del lugar", text: $viewModel.name)
+                    TextField("Descripción", text: $viewModel.description)
                 }
                 Section("Cerca de ti…") {
-                    switch loadingState {
+                    switch viewModel.loadingState {
                     case .loaded:
-                        ForEach(pages, id: \.pageid) { page in
+                        ForEach(viewModel.pages, id: \.pageid) { page in
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ") +
@@ -55,51 +45,23 @@ struct EditView: View {
             }
             .navigationTitle("Detalles del lugar")
             .toolbar {
-                Button("Guardar") {
-                    var newLocation = location
-                    newLocation.id = UUID()
-                    newLocation.name = name
-                    newLocation.description = description
-
+                Button("Save") {
+                    let newLocation = viewModel.createNewLocation()
                     onSave(newLocation)
                     dismiss()
                 }
             }
             .task {
-                await fetchNearbyPlaces()
+                await viewModel.fetchNearbyPlaces()
             }
         }
     }
     
     init(location: Location, onSave: @escaping (Location) -> Void) {
-        self.location = location
         self.onSave = onSave
-
-        _name = State(initialValue: location.name)
-        _description = State(initialValue: location.description)
+        _viewModel = State(initialValue: ViewModel(location: location))
     }
-    func fetchNearbyPlaces() async {
-        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
-
-        guard let url = URL(string: urlString) else {
-            print("Bad URL: \(urlString)")
-            return
-        }
-
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-
-            // we got some data back!
-            let items = try JSONDecoder().decode(Result.self, from: data)
-
-            // success – convert the array values to our pages array
-            pages = items.query.pages.values.sorted()
-            loadingState = .loaded
-        } catch {
-            // if we're still here it means the request failed somehow
-            loadingState = .failed
-        }
-    }
+    
 }
 
 #Preview {
